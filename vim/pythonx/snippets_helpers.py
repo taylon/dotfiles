4c018f53c3298eb8c6cca324287cb8936d9ac0a5
Taylon Silmer <taylonsilva@gmail.com>
2019-08-13 09:50:16 -0700

    Vim: Separate testing specific snippets into their own files

--- vim/pythonx/snippets_helpers.py
+++ vim/pythonx/snippets_helpers.py
@@ -1,2 +1,4 @@
-def remove_extension(file_name: str):
-    return file_name.split('.', 2)[0]
+import re
+
+def is_test_file(file_name: str):
+    return re.match('.+[\._]{1}test[\._]{1}', file_name)
