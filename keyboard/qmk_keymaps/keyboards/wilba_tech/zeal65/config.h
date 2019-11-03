0f31788ea6f66388520aafcc9c341ab22690ce5c
Taylon Silmer <taylonsilva@gmail.com>
2019-11-03 14:01:38 -0800

    Keyboards: Add Zeal65 keymap and update Koyu keymap

---
+++ keyboard/qmk_keymaps/keyboards/wilba_tech/zeal65/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define TAPPING_TERM 200
+#define IGNORE_MOD_TAP_INTERRUPT
+
+/* enable/disable LEDs based on layout */
+#undef RGB_BACKLIGHT_USE_SPLIT_BACKSPACE
+#define RGB_BACKLIGHT_USE_SPLIT_BACKSPACE 0
