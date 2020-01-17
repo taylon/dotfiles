9182d69677b5f16e0d66c3a35de64e87a8950a76
Taylon Silmer <taylonsilva@gmail.com>
2020-01-17 11:44:56 -0800

    Keyboards: Update Dactyl's keymap

--- keyboard/firmware/qmk/keyboards/handwired/dactyl/rules.mk
+++ keyboard/firmware/qmk/keyboards/handwired/dactyl/rules.mk
@@ -1,6 +1,6 @@
 TAP_DANCE_ENABLE = yes
 
-CONSOLE_ENABLE = yes # Console for debug(+400)
+CONSOLE_ENABLE = no # Console for debug(+400)
 COMMAND_ENABLE = no  # Commands for debug and configuration
 
-MOUSEKEY_ENABLE = yes
+MOUSEKEY_ENABLE = no
