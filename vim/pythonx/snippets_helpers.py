cb3a0ebc956df640f1f7ef3531c20dc8be54fe37
Taylon Silva <taylonsilva@gmail.com>
2020-09-14 19:44:45 -0700

    Vim: Improve ReasonML snippets

--- vim/pythonx/snippets_helpers.py
+++ vim/pythonx/snippets_helpers.py
@@ -1,4 +1,14 @@
 import re
+import inflect
+
+p = inflect.engine()
+
 
 def is_test_file(file_name: str):
-    return re.match('.+[\._]{1}test[\._]{1}', file_name)
+    return re.match(".+[\._]{1}test[\._]{1}", file_name)
+
+
+def singular_noun(noun: str):
+    value = p.singular_noun(noun) if len(noun) > 0 else ""
+
+    return value if value else ""
