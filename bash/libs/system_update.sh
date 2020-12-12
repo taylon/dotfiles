7ca63a432f41ec04679cfa7622ccc7852c8e0afc
Taylon Silva <taylonsilva@gmail.com>
2020-12-12 12:36:39 -0800

    Arch: update update script

--- bash/libs/system_update.sh
+++ bash/libs/system_update.sh
@@ -5,7 +5,7 @@ source /home/taylon/Development/dotfiles/bash/libs/print.sh
 system_update::common() {
   echo
   print::note "Updating Fish plugins..."
-  fish -c fisher update
+  fish -c "fisher update"
 
   echo
   print::note "Updating global Node packages"
